26478f99ad2ff2596b7b1637099cdaef97596429
Kajonczyk <kopczackikamil2000@gmail.com>
2023-04-04 21:21:51 +0200

    [JS-001] Added arePasswordsEqual function

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -14,11 +14,20 @@ export class RegisterComponent implements OnInit{
 		this.registerForm = new FormGroup({
 			email: new FormControl('', [Validators.required, Validators.email]),
 			login: new FormControl('', [Validators.required, Validators.minLength(3)]),
-			password: new FormControl('', [Validators.required, Validators.minLength(8)]),
-			repeatPassword: new FormControl('', [Validators.required, Validators.minLength(8)]),
+			password: new FormControl('', [Validators.required, Validators.minLength(8), this.arePasswordsEqual()]),
+			repeatPassword: new FormControl('', [Validators.required, Validators.minLength(8), this.arePasswordsEqual()]),
 			firstName: new FormControl('', Validators.required),
 			lastName: new FormControl('', Validators.required),
-		}, )
+		} )
+	}
+
+
+	arePasswordsEqual(): ValidatorFn{
+		return () => {
+			const pwdValue = this.registerForm?.controls["password"].value;
+			const repeatPwdValue = this.registerForm?.controls["repeatPassword"].value;
+			return pwdValue !== repeatPwdValue ? {pwdMismatch: true} : null
+		}
 	}
 
 	onSubmit(e: MouseEvent){
