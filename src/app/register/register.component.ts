c6fcdb90a2268bf93411eddd2135f83428b484c9
Kajonczyk <kopczackikamil2000@gmail.com>
2023-04-05 15:43:48 +0200

    [JS-001] Added pwd match validation

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,12 +1,12 @@
 import {Component, OnInit} from '@angular/core';
-import {AbstractControl, FormControl, FormGroup, ValidatorFn, Validators} from '@angular/forms';
+import {FormControl, FormGroup, ValidatorFn, Validators} from '@angular/forms';
 
 @Component({
 	selector: 'app-register',
 	templateUrl: './register.component.html',
 	styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit{
+export class RegisterComponent implements OnInit {
 
 	registerForm: FormGroup;
 
@@ -14,24 +14,36 @@ export class RegisterComponent implements OnInit{
 		this.registerForm = new FormGroup({
 			email: new FormControl('', [Validators.required, Validators.email]),
 			login: new FormControl('', [Validators.required, Validators.minLength(3)]),
-			password: new FormControl('', [Validators.required, Validators.minLength(8), this.arePasswordsEqual()]),
-			repeatPassword: new FormControl('', [Validators.required, Validators.minLength(8), this.arePasswordsEqual()]),
+			password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+			repeatPassword: new FormControl('', [Validators.required, Validators.minLength(8)]),
 			firstName: new FormControl('', Validators.required),
-			lastName: new FormControl('', Validators.required),
-		} )
+			lastName: new FormControl('', Validators.required)
+		}, [this.arePasswordsEqual()]);
 	}
 
 
-	arePasswordsEqual(): ValidatorFn{
-		return () => {
-			const pwdValue = this.registerForm?.controls["password"].value;
-			const repeatPwdValue = this.registerForm?.controls["repeatPassword"].value;
-			return pwdValue !== repeatPwdValue ? {pwdMismatch: true} : null
+	arePasswordsEqual(): ValidatorFn {
+		return (formGroup) => {
+			const passwordControl = formGroup?.get('password');
+			const repeatPasswordControl = formGroup?.get('repeatPassword');
+			const doPasswordsMismatch = passwordControl?.value !== repeatPasswordControl?.value;
+
+			passwordControl?.setErrors(doPasswordsMismatch ? {pwdMismatch: true} : null);
+			repeatPasswordControl?.setErrors(doPasswordsMismatch ? {pwdMismatch: true} : null);
+
+			return null;
+		};
+	}
+
+	onSubmit(e: MouseEvent) {
+		e.preventDefault();
+		if (this.registerForm.valid) {
+			console.log('SUBMIT');
+		} else {
+			this.registerForm.markAllAsTouched();
+			this.registerForm.setErrors([{...this.registerForm.errors}]);
 		}
-	}
-
-	onSubmit(e: MouseEvent){
-		e.preventDefault()
+		console.log(this.registerForm.valid, this.registerForm.errors);
 	}
 
 
